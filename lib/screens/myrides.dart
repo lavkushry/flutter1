f206ac20bc300469ed656badc48b1a9d8856f5d4
lavkushry <lavkushry@gmail.com>
2024-04-01 12:01:13 +0530

    addes next

--- lib/screens/myrides.dart
+++ lib/screens/myrides.dart
@@ -1,12 +1,35 @@
+import 'package:flutter/material.dart';
 
-
-
-
-class myrides extends StatelessWidget {
+class myrides extends StatefulWidget {
   const myrides({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    throw UnimplementedError();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black,),
+          onPressed: () {  },
+        ),
+        title: Text("My Rides"),
+        centerTitle: true,
+      ),
+      body:Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            ListView.builder(
+              itemBuilder: ,
+              itemCount: 8,)
+          ],
+        ),
+
+      ),
+    );
   }
+
 }
